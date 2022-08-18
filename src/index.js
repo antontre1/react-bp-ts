96a9786276dbda8028bb4d690cd1e282a45e9008
Antonin Folliasson <antonin@aitio.co>
2022-08-18 23:15:03 +0200

    ts ready

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+import App from './App'
+import Madiv from './Madiv'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <Madiv />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
